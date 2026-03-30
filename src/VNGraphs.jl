87a746a218d3ff48d11ba13ab2935d40a349754b
Mahmud Bello <blinkztyler@gmail.com>
2026-03-30 11:11:20 +0100

    fix :ci failures

--- src/VNGraphs.jl
+++ src/VNGraphs.jl
@@ -42,8 +42,25 @@ graph_add_edge(g::VNGraph,i::Integer,j::Integer) = c"graph_add_edge"(g.ptr,i,j)
 graph_del_edge(g::VNGraph,i::Integer,j::Integer) = c"graph_del_edge"(g.ptr,i,j)
 graph_has_edge(g::VNGraph,i::Integer,j::Integer) = c"graph_has_edge"(g.ptr,i,j)
 graph_add_node(g::VNGraph) = c"graph_add_node"(g.ptr)
-nnodes(g::VNGraph) = g.ptr.nnodes[] # c"nnodes"(g.ptr)
-nedges(g::VNGraph) = g.ptr.nedges[] # c"nedges"(g.ptr)
+nnodes(g::VNGraph) = Int(unsafe_load(reinterpret(Ptr{Cuint}, Base.unsafe_convert(Ptr{Cvoid}, g.ptr)), 13)) # Offset 48
+nedges(g::VNGraph) = Int(unsafe_load(reinterpret(Ptr{Cuint}, Base.unsafe_convert(Ptr{Cvoid}, g.ptr)), 14)) # Offset 52
+
+# Robust field accessors for pointers (8-byte aligned)
+function get_d_ptr(g::VNGraph)
+    return unsafe_load(reinterpret(Ptr{Ptr{Cuint}}, Base.unsafe_convert(Ptr{Cvoid}, g.ptr)), 2) # Offset 8
+end
+
+function get_a_ptr(g::VNGraph)
+    return unsafe_load(reinterpret(Ptr{Ptr{Ptr{Cuint}}}, Base.unsafe_convert(Ptr{Cvoid}, g.ptr)), 1) # Offset 0
+end
+
+function get_c_ptr(g::VNGraph)
+    return unsafe_load(reinterpret(Ptr{Ptr{Cint}}, Base.unsafe_convert(Ptr{Cvoid}, g.ptr)), 5) # Offset 32
+end
+
+function get_l_ptr(g::VNGraph)
+    return unsafe_load(reinterpret(Ptr{Ptr{Cint}}, Base.unsafe_convert(Ptr{Cvoid}, g.ptr)), 6) # Offset 40
+end
 
 graph_node_degree(g::VNGraph, i::Integer) = c"graph_node_degree"(g.ptr, i)
 graph_min_degree(g::VNGraph) = c"graph_min_degree"(g.ptr)
@@ -52,10 +69,10 @@ graph_mean_degree(g::VNGraph) = c"graph_mean_degree"(g.ptr)
 
 graph_show(g::VNGraph) = c"graph_show"(g.ptr)
 
-graph_nclusters(g::VNGraph) = c"graph_nclusters"(g.ptr)
-graph_connected(g::VNGraph) = c"graph_connected"(g.ptr)
+graph_nclusters(g::VNGraph) = Int(c"graph_nclusters"(g.ptr))
+graph_connected(g::VNGraph) = c"graph_connected"(g.ptr) != 0
 
-cluster(g::VNGraph,i::Integer) = g.ptr.l[][i]
+cluster(g::VNGraph,i::Integer) = unsafe_load(get_l_ptr(g), i+1)
 graph_cluster_sizes(g::VNGraph) = c"graph_cluster_sizes"(g.ptr)
 graph_max_cluster(g::VNGraph) = c"graph_max_cluster"(g.ptr)
 
@@ -77,7 +94,7 @@ graph_local_complement(g::VNGraph, i::Integer) = c"graph_local_complement"(g.ptr
 graph_sequential_color_repeat(g::VNGraph, n::Integer) = c"graph_sequential_color_repeat"(g.ptr, n)
 graph_chromatic_number(g::VNGraph, timeout) = c"graph_chromatic_number"(g.ptr, timeout)
 graph_edge_chromatic_number(g::VNGraph, timeout) = c"graph_edge_chromatic_number"(g.ptr, timeout)
-color(g::VNGraph,i) = g.ptr.c[][i]
+color(g::VNGraph,i) = unsafe_load(get_c_ptr(g), i+1)
 graph_ncolors(g::VNGraph) = c"graph_ncolors"(g.ptr)
 graph_check_coloring(g::VNGraph) = c"graph_check_coloring"(g.ptr)
 
@@ -85,11 +102,14 @@ graph_check_coloring(g::VNGraph) = c"graph_check_coloring"(g.ptr)
 function Graphs.SimpleGraphs.SimpleGraph(vng::VNGraph)
     n = nnodes(vng)
     # Build fadjlist directly for speed
-    fadjlist = [Vector{Int}(undef, vng.ptr.d[][i-1]) for i in 1:n]
+    d_ptr = get_d_ptr(vng)
+    a_ptr = get_a_ptr(vng)
+    fadjlist = [Vector{Int}(undef, unsafe_load(d_ptr, i)) for i in 1:n]
     for i in 1:n
-        d = vng.ptr.d[][i-1]
+        d = unsafe_load(d_ptr, i)
+        a_i_ptr = unsafe_load(a_ptr, i)
         for k in 1:d
-            fadjlist[i][k] = vng.ptr.a[][i-1][k-1] + 1
+            fadjlist[i][k] = unsafe_load(a_i_ptr, k) + 1
         end
         sort!(fadjlist[i])
     end
@@ -119,10 +139,13 @@ function Base.iterate(it::VNEdgeIterator, state=(1, 1))
     i, k = state
     n = nnodes(g)
     
+    d_ptr = get_d_ptr(g)
+    a_ptr = get_a_ptr(g)
     while i <= n
-        d = g.ptr.d[][i-1]
+        d = unsafe_load(d_ptr, i)
         while k <= d
-            j = g.ptr.a[][i-1][k-1] + 1
+            a_i_ptr = unsafe_load(a_ptr, i)
+            j = unsafe_load(a_i_ptr, k) + 1
             if i < j
                 return (Graphs.SimpleGraphs.SimpleEdge{Cuint}(i, j), (i, k + 1))
             end
@@ -139,15 +162,16 @@ Graphs.edgetype(g::VNGraph) = Graphs.SimpleGraphs.SimpleEdge{eltype(g)}
 
 function Graphs.has_edge(g::VNGraph, s::Integer, d::Integer)
     (s < 1 || s > nnodes(g) || d < 1 || d > nnodes(g)) && return false
-    return graph_has_edge(g, s-1, d-1)
+    return graph_has_edge(g, s-1, d-1) != 0
 end
 
 Graphs.has_vertex(g::VNGraph, n::Integer) = 1≤n≤nnodes(g)
 
 function Graphs.outneighbors(g::VNGraph, v::Integer)
     (v < 1 || v > nnodes(g)) && return Cuint[]
-    d = g.ptr.d[][v-1]
-    return [g.ptr.a[][v-1][k-1] + 1 for k in 1:d]
+    d = unsafe_load(get_d_ptr(g), v)
+    a_v_ptr = unsafe_load(get_a_ptr(g), v)
+    return [unsafe_load(a_v_ptr, k) + 1 for k in 1:d]
 end
 
 Graphs.inneighbors(g::VNGraph, v::Integer) = Graphs.outneighbors(g, v)
@@ -156,11 +180,13 @@ Graphs.neighbors(g::VNGraph, v::Integer) = Graphs.outneighbors(g, v)
 Graphs.is_directed(::Type{VNGraph}) = false
 Graphs.ne(g::VNGraph) = nedges(g)
 Graphs.nv(g::VNGraph) = nnodes(g)
-Graphs.vertices(g::VNGraph) = 1:nnodes(g)
+Graphs.vertices(g::VNGraph) = UnitRange{Cuint}(1, nnodes(g))
 
 function Graphs.add_edge!(g::VNGraph, s::Integer, d::Integer)
     (s < 1 || s > nnodes(g) || d < 1 || d > nnodes(g)) && return false
-    return graph_add_edge(g, s-1, d-1)
+    graph_has_edge(g, s-1, d-1) != 0 && return false
+    graph_add_edge(g, s-1, d-1)
+    return true
 end
 
 Graphs.add_edge!(g::VNGraph, e::Graphs.SimpleGraphEdge) = Graphs.add_edge!(g, e.src, e.dst)
@@ -172,12 +198,14 @@ end
 
 function Graphs.rem_edge!(g::VNGraph, s::Integer, d::Integer)
     (s < 1 || s > nnodes(g) || d < 1 || d > nnodes(g)) && return false
-    return graph_del_edge(g, s-1, d-1)
+    graph_has_edge(g, s-1, d-1) == 0 && return false
+    graph_del_edge(g, s-1, d-1)
+    return true
 end
 
 function Graphs.degree(g::VNGraph, v::Integer)
     (v < 1 || v > nnodes(g)) && return 0
-    return Int(g.ptr.d[][v-1])
+    return Int(c"graph_node_degree"(g.ptr, v-1))
 end
 
 # Algorithm dispatch
@@ -185,17 +213,20 @@ struct VNAlgorithm end
 export VNAlgorithm
 
 Graphs.clique_number(g::VNGraph) = graph_clique_number(g)
+Graphs.clique_number(g::VNGraph, ::VNAlgorithm) = Graphs.clique_number(g)
 Graphs.clique_number(g::Graphs.AbstractGraph, ::VNAlgorithm) = graph_clique_number(VNGraph(g))
 
-function Graphs.chromatic_number(g::VNGraph, timeout=0)
+function Graphs.chromatic_number(g::VNGraph; timeout=0)
     return graph_chromatic_number(g, timeout)
 end
-Graphs.chromatic_number(g::Graphs.AbstractGraph, ::VNAlgorithm; timeout=0) = Graphs.chromatic_number(VNGraph(g), timeout)
+Graphs.chromatic_number(g::VNGraph, ::VNAlgorithm; timeout=0) = Graphs.chromatic_number(g; timeout=timeout)
+Graphs.chromatic_number(g::Graphs.AbstractGraph, ::VNAlgorithm; timeout=0) = Graphs.chromatic_number(VNGraph(g); timeout=timeout)
 
-function Graphs.edge_chromatic_number(g::VNGraph, timeout=0)
+function Graphs.edge_chromatic_number(g::VNGraph; timeout=0)
     return graph_edge_chromatic_number(g, timeout)
 end
-Graphs.edge_chromatic_number(g::Graphs.AbstractGraph, ::VNAlgorithm; timeout=0) = Graphs.edge_chromatic_number(VNGraph(g), timeout)
+Graphs.edge_chromatic_number(g::VNGraph, ::VNAlgorithm; timeout=0) = Graphs.edge_chromatic_number(g; timeout=timeout)
+Graphs.edge_chromatic_number(g::Graphs.AbstractGraph, ::VNAlgorithm; timeout=0) = Graphs.edge_chromatic_number(VNGraph(g); timeout=timeout)
 
 function Graphs.connected_components(g::VNGraph)
     n = nnodes(g)
